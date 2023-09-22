ad07139474657b7050aed9ec453b714a3a430240
Neeraj Manchanda <manchanda2612@gmail.com>
2023-09-22 15:29:47 +0530

    few changes done

--- app/src/main/java/com/neeraj/booksapp/BookApplication.kt
+++ app/src/main/java/com/neeraj/booksapp/BookApplication.kt
@@ -7,19 +7,5 @@ import dagger.hilt.android.HiltAndroidApp
 /**
  * @author Neeraj Manchanda
  */
-@Suppress("unused")
 @HiltAndroidApp
-class BookApplication : Application() {
-    init {
-        instance = this
-    }
-
-    companion object {
-
-        private var instance: BookApplication? = null
-
-        fun applicationContext(): BookApplication {
-            return instance as BookApplication
-        }
-    }
-}
+class BookApplication : Application()
